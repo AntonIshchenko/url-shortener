ce8bc5400503cf43622ca5b548d822b4367c81ac
Anton Ishchenko <anton.ishchenko@coxautoinc.com>
2024-03-13 12:26:35 +0200

    added config file and retreiving config

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// TODO: init config: cleanenv
+
+	// TODO: init logger: slog -> log/slog package
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi -> net/http "chi render"
+
+	// TODO: tun server
+
+}
